96a44e2082482a675833d969ecc2f59b0722e5f4
Ripca <ricardoarrecisrivera@gmail.com>
2026-05-23 23:29:12 -0600

    antes de cambio db

--- ProyectoEjemplo/generate_hashes.py
+++
@@ -1,6 +0,0 @@
-import bcrypt
-
-passwords = ["admin123", "docente123"]
-for pwd in passwords:
-    hashed = bcrypt.hashpw(pwd.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-    print(f"{pwd}: {hashed}")
